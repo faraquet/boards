4de89c8828c250868ec1a5c5a3c51e89c9a2aaa0
Andrey Andriychuk <a.andriychuk@socialquantum.com>
2018-06-30 10:41:12 +0300

    Add some styles

--- app/models/user_board.rb
+++ app/models/user_board.rb
@@ -1,5 +1,11 @@
 class UserBoard < ApplicationRecord
   scope :uncompleted, -> { where(completed_at: nil) }
+  validates :question, presence: true
 
   has_many :answers
+
+  def name
+    name = super
+    name.presence || 'Anonymous'
+  end
 end
